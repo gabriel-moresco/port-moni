244bfeee16c4d040b465fc03e86872223001e6b0
Gabriel Moresco <gabriel.moresco25@gmail.com>
2020-01-29 21:35:39 -0300

    Commit Inicial

---
+++ PortMoni/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+
+namespace PortMoni
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            try
+            {
+                new MainWindow().ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fatal Error: " + ex.Message, "Ops!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ PortMoni/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using MahApps.Metro.Controls;
+
+namespace PortMoni
+{
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
