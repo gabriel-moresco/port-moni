95697504947255e04103f8394215718bfaade65e
Gabriel Moresco <gabriel.moresco25@gmail.com>
2020-02-01 10:19:46 -0300

    Removido classes desnecessárias | Alterado classe Util para o projeto UTIL

--- PortMoni.SERVICES/QuoteServices.cs
+++ PortMoni.SERVICES/QuoteServices.cs
@@ -1,4 +1,5 @@
 ﻿using PortMoni.MODEL;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
@@ -11,17 +12,24 @@ namespace PortMoni.SERVICES
         {
             string response = string.Empty;
 
-            //http://bvmf.bmfbovespa.com.br/cotacoes2000/FormConsultaCotacoes.asp?strListaCodigos=BBPO11%7CPETR4
-            string requestString = "http://bvmf.bmfbovespa.com.br/cotacoes2000/FormConsultaCotacoes.asp?strListaCodigos=";
-
-            foreach (Share share in shareList)
+            try
             {
-                requestString += share.Code + "%7C";
-            }
+                //http://bvmf.bmfbovespa.com.br/cotacoes2000/FormConsultaCotacoes.asp?strListaCodigos=BBPO11%7CPETR4
+                string requestString = "http://bvmf.bmfbovespa.com.br/cotacoes2000/FormConsultaCotacoes.asp?strListaCodigos=";
 
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(requestString);
-            HttpWebResponse httpWebResponse = (HttpWebResponse)request.GetResponse();
-            response = new StreamReader(httpWebResponse.GetResponseStream()).ReadToEnd();
+                foreach (Share share in shareList)
+                {
+                    requestString += share.Code + "%7C";
+                }
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(requestString);
+                HttpWebResponse httpWebResponse = (HttpWebResponse)request.GetResponse();
+                response = new StreamReader(httpWebResponse.GetResponseStream()).ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro ao obter valor das cotações do servidor da B3:\n" + ex.Message);
+            }
 
             return response;
         }
